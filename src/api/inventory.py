2499c8a0ac10e2dfbb4e2347e69e635f5a219a5c
Jonathan DeBarro <jdebarro@calpoly.edu>
2024-04-05 19:48:34 -0700

    Added inventory audit functionality

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from src.api import auth
 import math
+import sqlalchemy
+from src import database as db
 
 router = APIRouter(
     prefix="/inventory",
@@ -12,8 +14,11 @@ router = APIRouter(
 @router.get("/audit")
 def get_inventory():
     """ """
-    
-    return {"number_of_potions": 0, "ml_in_barrels": 0, "gold": 0}
+    sql_to_execute = "SELECT * FROM global_inventory"
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text(sql_to_execute)).fetchall()[0]
+        #(id, num_green_potions, num_green_ml, gold)
+    return {"number_of_potions": result[1], "ml_in_barrels": result[2], "gold": result[3]}
 
 # Gets called once a day
 @router.post("/plan")

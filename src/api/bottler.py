f65f0f7b13b51a7ef0417bf94607de4f755053ed
Jonathan DeBarro <jdebarro@calpoly.edu>
2024-04-10 17:19:27 -0700

    moved bulk of bottle del into helper

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -21,36 +21,37 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int
     """ """
     if len(potions_delivered) == 0:
         print("No potions delivered")
-
     else:
         red, green, blue = getMl()
-        with db.engine.begin() as connection:
-            for potion in potions_delivered:
-                print(f"Pre Red: {red} | Blue: {blue} | Green: {green}")
-                print(potion)
-                red -= potion.potion_type[0] * potion.quantity
-                green -= potion.potion_type[1] * potion.quantity
-                blue -= potion.potion_type[2] * potion.quantity
-
-                sql_to_execute = f"SELECT * FROM potions_table WHERE red = {potion.potion_type[0]} AND green = {potion.potion_type[1]} AND blue = {potion.potion_type[2]}"
-                result = connection.execute(sqlalchemy.text(sql_to_execute)).fetchall()[0]
-                print(result)
-                #Check if row exists
-                if result:
-                    cur_quantity = result.quantity + potion.quantity
-                    sql_to_execute = f"UPDATE potions_table SET quantity = {cur_quantity} WHERE sku = '{result.sku}'"
-                    update = connection.execute(sqlalchemy.text(sql_to_execute))
-                else:
-                    # Not relevant until we do unique potions
-                    continue
-            # Subtract ml used for potions
-            print(f"Post Red: {red} | Blue: {blue} | Green: {green}")
-            sql_to_execute = f"UPDATE global_inventory SET num_green_ml = {green}, num_red_ml = {red}, num_blue_ml = {blue}"
-            result = connection.execute(sqlalchemy.text(sql_to_execute))
-            
+        deliverPotions(red, green, blue, potions_delivered)    
         print(f"potions delievered: {potions_delivered} order_id: {order_id}")
 
     return "OK"
+
+
+def deliverPotions(red, green, blue, potions_delivered):
+    '''Handles SQL UPDATES for potion delivery'''
+    with db.engine.begin() as connection:
+        for potion in potions_delivered:
+            red -= potion.potion_type[0] * potion.quantity
+            green -= potion.potion_type[1] * potion.quantity
+            blue -= potion.potion_type[2] * potion.quantity
+
+            sql_to_execute = f"SELECT * FROM potions_table WHERE red = {potion.potion_type[0]} AND green = {potion.potion_type[1]} AND blue = {potion.potion_type[2]}"
+            result = connection.execute(sqlalchemy.text(sql_to_execute)).fetchall()[0]
+            print(result)
+            #Check if row exists
+            if result:
+                cur_quantity = result.quantity + potion.quantity
+                sql_to_execute = f"UPDATE potions_table SET quantity = {cur_quantity} WHERE sku = '{result.sku}'"
+                update = connection.execute(sqlalchemy.text(sql_to_execute))
+            else:
+                # Not relevant until we do unique potions
+                continue
+    # Subtract ml used for potions
+    sql_to_execute = f"UPDATE global_inventory SET num_green_ml = {green}, num_red_ml = {red}, num_blue_ml = {blue}"
+    result = connection.execute(sqlalchemy.text(sql_to_execute))
+
 
 def getMl():
     '''Returns current ml for every color and gold'''
